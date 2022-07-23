5d8a075dc958dc6771c17961e7fc05cf078eb4ea
odong-Tree <odongnamu.dev@gmail.com>
2022-07-23 15:16:48 +0900

    first commit

---
+++ Picterest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Picterest
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
